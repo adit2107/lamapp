c8e6ba6db81b10c28d815ee4209e9d014f48f940
adit2107 <blackice9107@gmail.com>
2020-03-20 20:14:57 +0530

    Add jumbo

--- router/main.js
+++ router/main.js
@@ -3,14 +3,17 @@ const conn  = require('../app');
 module.exports = function(app)
 {
      app.get('/',function(req,res){
-         conn.connection.query('SELECT * FROM malls.malls LIMIT 10', (error, results, fields) => {
-             if (error) throw error;
-             console.log(JSON.stringify(results));
-         });
-          res.render('pages/index.ejs',{connection: "Please login"})
+        res.render('pages/index.ejs',{connection: "Connected"})
      });
 
-     app.get('/about',function(req,res){
+     app.get('/login',function(req,res){
         res.render('about.ejs');
+    });
+
+    app.get('/listall', (req, res) => {
+        conn.connection.query('SELECT * FROM malls.malls LIMIT 10', (error, results, fields) => {
+            if (error) throw error;
+            console.log(JSON.stringify(results));
+        });
     });
 }
