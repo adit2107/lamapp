19cb8c70e9cf179763fc927b00621c4282f07919
adit2107 <blackice9107@gmail.com>
2020-03-22 11:54:16 +0530

    Add column filter

--- public/js/table.js
+++ public/js/table.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
-    $('#table').DataTable();
+
+    $('#table thead tr').clone(true).appendTo( '#table thead' );
+    $('#table thead tr:eq(1) th').each( function (i) {
+        var title = $(this).text();
+        $(this).html( '<input type="text" placeholder="Search" />' );
+ 
+        $( 'input', this ).on( 'keyup change', function () {
+            if ( table.column(i).search() !== this.value ) {
+                table
+                    .column(i)
+                    .search( this.value )
+                    .draw();
+            }
+        } );
+    } );
+ 
+    var table = $('#table').DataTable( {
+        orderCellsTop: true,
+        fixedHeader: true
+    } );
+    
 } );
