41b0a9416b9decac26828b11b1fb21c2ba06c4dd
adit2107 <blackice9107@gmail.com>
2020-04-14 15:19:47 +0530

    Add autocomp

--- app.js
+++ app.js
@@ -33,8 +33,8 @@ const connection = mysql.createConnection({
 });
   
 
-const server = app.listen(3000, () => {
-    console.log("Server running on 3000!");
+const server = app.listen(3030, () => {
+    console.log("Server running on 3030!");
 })
 
 exports.connection = connection;
--- router/main.js
+++ router/main.js
@@ -18,18 +18,32 @@ module.exports = function(app)
     app.get('/list', (req, res) => {
         conn.connection.query('SELECT * FROM malls.malls', (error, results, fields) => {
             if (error) throw error;
-            console.log(JSON.stringify(results));
             res.render('pages/list.ejs', {results: results})
         });
     });
 // TODO: Insert function
     app.get('/insert', (req, res) => {
-        res.render('pages/insert.ejs')
         conn.connection.query('SELECT * FROM malls.malls', (error, results, fields) => {
             if (error) throw error;
-        });
-        
+            let mallnames = [];
+            let stores = [];
+            let floor = [];
+            let category = [];
+            let dist = [];
+            let circle = [];
 
+            for (var i=0;i<results.length;i++){
+                mallnames.push(results[i].mallname)
+                stores.push(results[i].stores)
+                floor.push(results[i].floor)
+                category.push(results[i].category)
+                dist.push(results[i].distribution)
+                circle.push(results[i].circle)
+            }
+            
+            res.render('pages/insert.ejs', {results: results, mallnames: [ ...new Set(mallnames)], stores: [ ...new Set(stores)], floor: floor, category: [ ...new Set(category)], dist: [ ...new Set(dist)], circle: [ ...new Set(circle)]})
+
+        });
                 
     });
 }
