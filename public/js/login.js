214bd89b191513f9057c1d1a11daf43f8a2d57e0
adit2107 <blackice9107@gmail.com>
2020-04-22 17:41:28 +0530

    Add login page/button

--- public/js/login.js
+++ public/js/login.js
@@ -1,9 +1,4 @@
-document.getElementById("loginbtn").addEventListener("click" , () => {
-fetch(`https://mallapp21.auth.us-east-1.amazoncognito.com/login?response_type=code&client_id=${process.env.cog_client_id}&redirect_uri=http://localhost:3030/&state=STATE&scope=openid+profile+aws.cognito.signin.user.admin`, {
-    method: 'GET'
-})
-.then(res => res.json())
-.then(response => {
-    console.log(response);
-})
-});
+function login(e){
+    e.preventDefault();  
+    window.location.replace(`https://${process.env.cog_client_name}.auth.${process.env.cog_region}.amazoncognito.com/login?response_type=code&client_id=${process.env.cog_client_id}&redirect_uri=${process.env.redirecturi}&state=STATE&scope=openid+profile+aws.cognito.signin.user.admin`);
+}
